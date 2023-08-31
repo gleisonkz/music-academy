511957a2a1bbb7d6884c1cfae4cd814f0835cabe
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2023-08-31 10:50:05 -0300

    :sparkles: feat: add ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { NZ_I18N, pt_BR } from 'ng-zorro-antd/i18n';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import pt from '@angular/common/locales/pt';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+
+registerLocaleData(pt);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: pt_BR }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/icons-provider.module.ts
@@ -0,0 +1,23 @@
+import { NZ_ICONS, NzIconModule } from 'ng-zorro-antd/icon';
+
+import { NgModule } from '@angular/core';
+import {
+  DashboardOutline,
+  FormOutline,
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+} from '@ant-design/icons-angular/icons';
+
+const icons = [
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  DashboardOutline,
+  FormOutline,
+];
+
+@NgModule({
+  imports: [NzIconModule],
+  exports: [NzIconModule],
+  providers: [{ provide: NZ_ICONS, useValue: icons }],
+})
+export class IconsProviderModule {}
